bfa525834e43e793e59ae3e69c60dc8c3fdb8194
إياد سغيري <85361584+ayadseghairi@users.noreply.github.com>
2022-10-12 15:05:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,29 @@ import psutil
 from threading import Thread
 import time
 def get_pid(name):
-        process_name = name
-        pid = None
-        for proc in psutil.process_iter():
-                if process_name in proc.name():
-                        pid = proc.pid
-        return pid
+	process_name = name
+	pid = None
+	for proc in psutil.process_iter():
+    		if process_name in proc.name():
+       			pid = proc.pid
+	return pid
 def stoptimebro(somepid):
-        p = psutil.Process(somepid)
-        p.suspend()
-        time.sleep(60)
-        p.resume()
-        time.sleep(20)
+	p = psutil.Process(somepid)
+	p.suspend()
+	time.sleep(60)
+	p.resume()
+	time.sleep(20)
 def client():
-        while 1:
-                pid_bro_cl = get_pid("client.exe")
-                if pid_bro_cl != None
-                        stoptimebro(pid_bro_cl)
-def Guardit()
-        while 1:
-                pid_bro_gu = get_pid("guardit.exe")
-                if pid_bro_gu != None
-                        stoptimebro(pid_bro_gu)
-
+	while 1:
+		pid_bro_cl = get_pid("client.exe")
+		if pid_bro_cl != None:
+			stoptimebro(pid_bro_cl)
+def Guardit():
+	while 1:
+		pid_bro_gu = get_pid("guardit.exe")
+		if pid_bro_gu != None:
+			stoptimebro(pid_bro_gu)
+print("Start Hacking time...")
 one_thread = Thread(target=client)
 tow_thread = Thread(target=Guardit)
 one_thread.start()
