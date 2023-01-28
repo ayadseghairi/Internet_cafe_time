c08355773179c8366f8b4875f5df211df14d81a2
Ayad seghairi <85361584+ayadseghairi@users.noreply.github.com>
2023-01-28 19:53:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,12 +31,12 @@ def start():
 	global p2
 	if state == True :
 		state = False
-		showinfo("Time","Time has Started")
-		labelText.set("Time is wirking")
+		showinfo("Time","time is running")
+		labelText.set("time is running")
 		p1.resume()
 		p2.resume()
 	elif state == False :
-		showinfo("Time","Time is wirking")
+		showinfo("Time","time is running")
 	else :
 		win.destroy()
 def stop():
@@ -44,22 +44,22 @@ def stop():
 	global p1
 	global p2
 	if state == True :
-		showinfo("Time","Time has Stped")
+		showinfo("Time","time has stopped")
 	elif state == False :
 		state = True
-		showinfo("Time","Time has Stped")
-		labelText.set("Time is stoped")
+		showinfo("Time","time has stopped")
+		labelText.set("time has stopped")
 		p1.suspend()
 		p2.suspend()
 	else :
 		win.destroy()
 labelText = tkinter.StringVar()
-labelText.set("Time is wirking")
+labelText.set("time is running")
 tkinter.Label(win,bg="black",fg="red",font=("Ink Free", 50),text="Ayad Seghairi").place(x=25,y=0)
 lipttxt = tkinter.Label(win,bg="black",fg="red",font=("Ink Free", 30),textvariable=labelText).place(x=100,y=180)
 Startbt= tkinter.Button(win,text="Start Time",font=("Ink Free", 30),bg="red",fg="Green", activebackground="Green", activeforeground="red",command=lambda:start()).place(x=20,y=240)
 stopbt= tkinter.Button(win,text="Stop Time",font=("Ink Free", 30),bg="Green",fg="red",activebackground="red", activeforeground="Green",command=lambda:stop()).place(x=260,y=240)
-################################################  folow us   ################################
+################################################  Follow us   ################################
 tkinter.Button(win,text="Facebook",width=16,bg="blue",fg="black",activebackground="black", activeforeground="blue",command=lambda:webbrowser.open("http://facebook.com/ayadseghairi.off")).place(x=0,y=400)
 tkinter.Button(win,text="Github",width=16,bg="black",fg="white",activebackground="white", activeforeground="black",command=lambda:webbrowser.open("http://github.com/ayadseghairi")).place(x=167,y=400)
 tkinter.Button(win,text="Instagram",width=16,bg="yellow",fg="black",activebackground="black", activeforeground="yellow",command=lambda:webbrowser.open("http://instagram.com/ayadseghairi.off")).place(x=334,y=400)
